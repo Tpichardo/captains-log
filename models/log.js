f8b353fa8c7afa02f6608ac270230a4bbf0f8c75
Jon Narine <jonnicwolf@gmail.com>
2021-06-08 16:36:18 -0400

    nodemon up and running | server res.send prints

---
+++ models/log.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+      captainName: "Phillips",
+      title: "Pirates",
+      post: "Look at me, I am the captain now.",
+      mistakesWereMadeToday: true,
+      daysSinceLastCrisis: 0,
+    },
+    {
+      captainName: "America",
+      title: "Nazis",
+      post: "That's America's ass",
+      mistakesWereMadeToday: false,
+      daysSinceLastCrisis: 100,
+    },
+    {
+      captainName: "Captain Jack Sparrow",
+      title: "Also Pirates",
+      post: "I've got a jar of dirt",
+      mistakesWereMadeToday: true,
+      daysSinceLastCrisis: 100,
+    },
+  ];
+  
+  
+  
